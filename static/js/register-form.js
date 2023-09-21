c7687d629af9a2c92b3bca696f1e51e91b3a0652
Abdul mobin Fata <AbdulMobinFata>
2023-09-21 19:34:42 +0330

    some styles and font size increase

--- static/js/register-form.js
+++ static/js/register-form.js
@@ -1,44 +1,37 @@
-// register-form.js
 document.addEventListener('DOMContentLoaded', function () {
     const form = document.querySelector('.needs-validation');
-    const invalidCheck = document.getElementById('invalidCheck');
+    const birthDateInput = document.getElementById('birth-date');
+    const guardianDetailsSection = document.querySelector('.guardian-details');
 
-    // Hide invalid feedback initially
-    const invalidFeedbacks = form.querySelectorAll('.invalid-feedback');
-    invalidFeedbacks.forEach(function (feedback) {
-        feedback.style.display = 'none';
-    });
-
-    // Listen for form submission
     form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-            // Prevent form submission if validation fails
-            event.preventDefault();
-            event.stopPropagation();
-
-            // Show invalid feedback
-            invalidFeedbacks.forEach(function (feedback) {
-                feedback.style.display = 'block';
-            });
-        }
-    });
-
-    // Listen for changes in input fields and hide associated invalid feedback when valid
-    const inputFields = form.querySelectorAll('input, select');
-    inputFields.forEach(function (input) {
-        input.addEventListener('input', function () {
-            const associatedFeedback = form.querySelector(`.invalid-feedback[data-for="${input.id}"]`);
-            if (input.checkValidity()) {
-                // Input is valid, hide associated feedback
-                if (associatedFeedback) {
-                    associatedFeedback.style.display = 'none';
-                }
-            } else {
-                // Input is invalid, show associated feedback
-                if (associatedFeedback) {
-                    associatedFeedback.style.display = 'block';
-                }
+        // Check if the student's age is less than 18
+        const age = calculateAge(birthDateInput.value);
+        if (age < 18) {
+            // If the student is under 18, validate the guardian details
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
             }
-        });
+        } else {
+            // If the student is 18 or older, do not validate guardian details
+            guardianDetailsSection.classList.remove('was-validated');
+        }
+
+        // Always add 'was-validated' class to the form
+        form.classList.add('was-validated');
     });
+
+    // Function to calculate age based on birth date
+    function calculateAge(birthDate) {
+        const today = new Date();
+        const birthDateObj = new Date(birthDate);
+        let age = today.getFullYear() - birthDateObj.getFullYear();
+        const monthDiff = today.getMonth() - birthDateObj.getMonth();
+
+        if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDateObj.getDate())) {
+            age--;
+        }
+
+        return age;
+    }
 });
